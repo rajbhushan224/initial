e7d4af8a867867d69079c824d2e6ff37011fcf31
Raj-bhushan <raj.bhushan@citymall.live>
2023-11-25 12:04:00 +0530

    raw files

---
+++ logic.js
@@ -0,0 +1,14 @@
+// function submitForm() {
+//     const name = document.getElementById('name').value;
+//     const age = document.getElementById('age').value;
+//     const ctc = document.getElementById('ctc').value;
+//     const department = document.getElementById('department').value;
+
+//     // Send data to the server or perform other actions
+//     console.log('Submitting form:', { name, age, ctc, department });
+// }
+
+function submitForm() {
+    const form = document.getElementById('userForm');
+    form.submit();
+}
