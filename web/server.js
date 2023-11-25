52a533a229d76c140be507824b740e82a01a99af
Raj-bhushan <raj.bhushan@citymall.live>
2023-11-25 11:52:53 +0530

    raw files

---
+++ web/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Client } = require('pg');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const client = new Client({
+    user: 'postgres',
+    host: '13.126.237.141',
+    database: 'postgres',
+    password: 'postgres123',
+    port: 5432,
+});
+
+client.connect();
+
+app.post('/submit', (req, res) => {
+    const { name, age, ctc, department } = req.body;
+
+    const query = 'INSERT INTO users (name, age, ctc, department) VALUES ($1, $2, $3, $4)';
+    const values = [name, age, ctc, department];
+
+    client.query(query, values, (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error saving data to the database');
+        } else {
+            console.log('Data saved to the database');
+            res.status(200).send('Data saved successfully');
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
