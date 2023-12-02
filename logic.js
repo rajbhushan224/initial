07b15a1b9ed75ab669915fce1bd9a182ca9261b5
Raj-bhushan <raj.bhushan@citymall.live>
2023-12-02 15:48:41 +0530

    changeX

--- logic.js
+++ logic.js
@@ -1,14 +1,79 @@
-// function submitForm() {
-//     const name = document.getElementById('name').value;
-//     const age = document.getElementById('age').value;
-//     const ctc = document.getElementById('ctc').value;
-//     const department = document.getElementById('department').value;
+// Fetch user data from the server using AJAX
+async function getUsers() {
+    try {
+        // Make a GET request to the server endpoint
+        const response = await fetch('http://3.111.36.30:3001/get_users');
 
-//     // Send data to the server or perform other actions
-//     console.log('Submitting form:', { name, age, ctc, department });
-// }
+        // Check if the response status is OK (200)
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status}`);
+        }
+
+        // Parse the JSON response
+        const users = await response.json();
+
+        // Display user data in the HTML container
+        displayUsers(users);
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+    }
+}
+
+// Display user data in the HTML container
+function displayUsers(users) {
+    var userContainer = document.getElementById("userContainer");
+
+    // Clear existing user blocks
+    while (userContainer.firstChild) {
+        userContainer.removeChild(userContainer.firstChild);
+    }
+
+    // Populate the container with user blocks
+    users.forEach(function(user) {
+        var userBlock = document.createElement("div");
+        userBlock.className = "user-block";
+
+        // Create an image element (placeholder image URL used)
+        var userImage = document.createElement("img");
+        userImage.src = "img.jpg"; // Placeholder image URL
+        userImage.alt = "User Image";
+        userImage.className = "user-image";
+
+        // Append image to the user block
+        userBlock.appendChild(userImage);
+
+        var table = document.createElement("table");
+        for (var key in user) {
+            var row = document.createElement("tr");
+
+            var headerCell = document.createElement("th");
+            headerCell.appendChild(document.createTextNode(key));
+            row.appendChild(headerCell);
+
+            var dataCell = document.createElement("td");
+            dataCell.appendChild(document.createTextNode(user[key]));
+            row.appendChild(dataCell);
+
+            table.appendChild(row);
+        }
+
+        // Append table to the user block
+        userBlock.appendChild(table);
+
+        // Append the user block to the container
+        userContainer.appendChild(userBlock);
+    });
+}
+
+function goToRegistrationPage() {
+    window.location.href = 'index1.html';
+}
+// Call the function to fetch and display user data
+getUsers();
+
 
 function submitForm() {
     const form = document.getElementById('userForm');
     form.submit();
+    window.location.href = 'index.html';
 }
--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const { Pool } = require('pg');
 const bodyParser = require('body-parser');
 const { Client } = require('pg');
+const cors = require('cors');
 
 const app = express();
 const port = 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 const client = new Client({
+    user: 'postgres',
+    host: '13.126.237.141',
+    database: 'postgres',
+    password: 'postgres123',
+    port: 5432,
+});
+
+const pool = new Pool({
     user: 'postgres',
     host: '13.126.237.141',
     database: 'postgres',
@@ -35,6 +46,64 @@ app.post('/submit', (req, res) => {
     });
 });
 
+app.get('/get_users', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM users');
+        const users = result.rows;
+        client.release();
+        res.json(users);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.use(express.static('public'));
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
+
+
+// const express = require('express');
+// const { Pool } = require('pg');
+// const cors = require('cors');
+
+// const app = express();
+// const port = 3001;
+
+// // Use CORS middleware
+// app.use(cors());
+
+// // Replace these with your PostgreSQL connection details
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: '13.126.237.141',
+//     database: 'postgres',
+//     password: 'postgres123',
+//     port: 5432,
+// });
+
+// // Endpoint to get user data
+// app.get('/get_users', async (req, res) => {
+//     try {
+//         const client = await pool.connect();
+//         const result = await client.query('SELECT * FROM users');
+//         const users = result.rows;
+//         client.release();
+//         res.json(users);
+//     } catch (err) {
+//         console.error(err);
+//         res.status(500).send('Internal Server Error');
+//     }
+// });
+
+// // Serve static files (e.g., HTML, CSS, JS)
+// app.use(express.static('public'));
+
+// // Start the server
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+// });
